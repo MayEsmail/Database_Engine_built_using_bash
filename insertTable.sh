8061577058a3935205d37ddd255ecc9688599857
AALAA <aalaa@localhost.localdomain>
2021-01-30 23:44:04 +0200

    insert into table version1

---
+++ insertTable.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+clear
+echo " ------------------------------------"
+echo "   Available Tables in ourDataBase   "
+ls tables
+echo "-------------------------------------"
+read -p "Enter table Name: " insertb
+if [[ -f tables/$insertb ]]
+then
+typeset  number_row=`awk 'END { print NR }' tables/$insertb".meta";`
+pk=`awk -F: '{print $1}' tables/$insertb`
+for(( i=0 ; i<$number_row ; i++ ))
+do
+	read -p "     Enter Data Of The $i Coulumn : " data
+	if [[ $i = 0 ]]
+	then 
+	typeset -i x=0
+		for var in `awk -F: '{print $1}' tables/$insertb`
+			do 
+				if [[ $var = $data ]]
+				then
+				let x=1
+				break
+				fi
+			done
+	
+
+		if [[ $x = 0 ]]
+			then
+			echo "*****Primary key is correct*******"
+			let x=1
+		else
+			echo "*****Duplicated Primary key********"
+			sleep 1
+			./.sh
+		fi
+
+	fi
+AppendedLine+="${data}:"
+
+done
+echo "${AppendedLine}">> tables/$insertb
+echo "----------------------------------------------"
+echo "             *** Add New Row Successfully *** "
+echo "----------------------------------------------"
+
+select var in "Insert Another Record" "Back"
+do
+case $var in
+"Insert Another Record")
+	../.././insertTable.sh
+;;
+
+"Back")
+../.././inner_menu.sh
+;;
+
+*)
+echo "Wrong choice"
+;;
+esac
+done
+fi
