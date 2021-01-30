92777ed6380ece04e465d243d9b649981ca5af4c
AALAA <aalaa@localhost.localdomain>
2021-01-30 09:51:08 +0200

    update crated_table

--- table_create.sh
+++ table_create.sh
@@ -1,26 +1,48 @@
 #!/bin/bash
-echo "------------------------------------"
-echo " create new table                   "
-echo "------------------------------------"
+typeset -i pk_selected=0
+clear;
+echo "********************************************"
+echo "             create new table               "
+echo "********************************************"
 read -p "Enter Table Name : " tbname
-#if ! [[ $tbname =~ ^[a-zA-Z]+$ ]];
-#then 
-#	echo "$tbname is not string value ,please inseart a valid one "	
-if [ -f ./$tbname ];
+if [[ -f tables/$tbname ]]
 then	
-	echo "Table $tbname already exists";
+	echo "Table $tbname already exists"
 else
-touch ./tables/$tbname;
-read -p "Enter Number of columns :" n;
+touch tables/$tbname".meta"
+touch tables/$tbname
+read -p "Enter Number of columns : " n
 for (( i = 1; i <= n; i++ ))
 do
-read -p "Enter column $i name :" name;
-echo -n $name":" >> ./tables/$tbname;
-read -p "Enter data type you wnat (int or string) :" columType;
-echo -n $columType":" >> ./tables/$tbname;
-read -p "(select As primary Key pk/not) :" is_pk;
-echo  $is_pk >> ./tables/$tbname;
+read -p "Enter column $i name : " name;
+echo -n $name":" >> tables/$tbname".meta"
+echo "Select the data type of $name"
+select x in "Int" "String"
+do
+case $REPLY in
+1 ) echo -n "int" >> tables/$tbname".meta";
+    break ;;
+2 ) echo -n "string" >> tables/$tbname".meta";
+    break ;;
+* ) echo "Invalid Choise .. Try Again, Enter the number corresponding to your choise" ;;
+esac
 done
-echo "$tbname has been created"
+if [ $pk_selected -eq 0 ]
+then
+echo "Do you want to make $name as the primary key?"
+select x in "YES" "NO"
+do
+case $REPLY in
+1 ) echo -n ":pk" >> tables/$tbname".meta";
+    pk_selected=1;
+    break ;;
+2 ) break ;;
+* ) echo "Invalid Choise .. Try Again, Enter the number corresponding to your choise" ;;
+esac
+done
 fi
-~            
+echo "" >> tables/$tbname".meta";
+done
+echo "          ####### Table $tbname has been created successfully ########"
+sleep 1
+fi
